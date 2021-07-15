c5a913a00eff692df2d543448a66d266ac2409d7
joshuacc1 <joshuacc1@gmail.com>
2021-07-15 10:40:51 -0400

    Update defaults.py
    Flake*

--- defaults.py
+++ defaults.py
@@ -4,13 +4,16 @@ import typing
 
 
 class JsonConfigHandler:
-    def __init__(self, config):
+    """Json Management class"""
+    
+    def __init__(self, config: str):
         self.config = config  # config file path
         self.default_dir = ""
         self.default_volume = 1.0
         self.load_and_read()
 
     def load_and_read(self) -> typing.List[typing.AnyStr]:
+        """loads and parses a JSON"""
         f = json.load(
             open(
                 f"{self.config}",
